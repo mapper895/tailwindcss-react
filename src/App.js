5358b431ad89d827c6876531b7d3c572cb71b00c
mapper895 <mapper.895.mp@gmail.com>
2021-05-08 16:33:15 -0500

    Dropdown Menu

--- src/App.js
+++ src/App.js
@@ -1,5 +1,7 @@
+import React, { useState, useEffect } from "react";
 import { Route, Switch } from "react-router-dom";
 import "./App.css";
+import Dropdown from "./components/Dropdown";
 import Footer from "./components/Footer";
 import Navbar from "./components/Navbar";
 import Home from "./pages";
@@ -7,9 +9,30 @@ import About from "./pages/about";
 import Menu from "./pages/menu";
 
 function App() {
+  const [isOpen, setIsOpen] = useState(false);
+
+  const toggle = () => {
+    setIsOpen(!isOpen);
+  };
+
+  useEffect(() => {
+    const hideMenu = () => {
+      if (window.innerWidth < 768 && isOpen) {
+        setIsOpen(false);
+      }
+    };
+
+    window.addEventListener("resize", hideMenu);
+
+    return () => {
+      window.addEventListener("resize", hideMenu);
+    };
+  });
+
   return (
     <>
-      <Navbar />
+      <Navbar toggle={toggle} />
+      <Dropdown isOpen={isOpen} toggle={toggle} />
       <Switch>
         <Route path="/" exact component={Home} />
         <Route path="/menu" component={Menu} />
